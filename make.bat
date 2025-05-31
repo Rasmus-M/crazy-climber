f4a6c1636bd142dd868a1feef5df778bc195879c
Rasmus Moustgaard <rasmus.moustgaard@gmail.com>
2025-05-31 17:49:30 +0200

    Scrolling

--- make.bat
+++ make.bat
@@ -4,8 +4,8 @@ rem xas99.py src/rom-banks.a99 -B -q -R -o bin/banks.bin
 java -jar tools/ea5tocart.jar bin\main "CRAZY CLIMBER"
 
 copy /b bin\main8.bin ^
-rem    + bin\banks.bin ^
     crazy-climber8.bin
+rem    + bin\banks.bin ^
 
 rem java -jar tools/CopyHeader.jar crazy-climber8.bin 60
 
