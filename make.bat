45d171ac54d8b87708bbf74eba3ca4e452f0acae
Rasmus Moustgaard <rasmus.moustgaard@gmail.com>
2025-09-08 20:36:33 +0200

    Title screen

--- make.bat
+++ make.bat
@@ -1,13 +1,16 @@
 xas99.py src/crazy-climber.a99 -i -q -R -L crazy-climber.lst -E symbols.txt -o bin/main
-rem xas99.py src/rom-banks.a99 -B -q -R -o bin/banks.bin
+xas99.py src/rom-bank-4.a99 -B -q -R -o bin/bank4.bin
 
 java -jar tools/ea5tocart.jar bin\main "CRAZY CLIMBER"
 
 copy /b bin\main8.bin ^
+    + bin\bank4.bin ^
+    + bin\empty.bin ^
+    + bin\empty.bin ^
+    + bin\empty.bin ^
     crazy-climber8.bin
-rem    + bin\banks.bin ^
 
-rem java -jar tools/CopyHeader.jar crazy-climber8.bin 60
+java -jar tools/CopyHeader.jar crazy-climber8.bin 60
 
 jar -cvf crazy-climber.rpk crazy-climber8.bin layout.xml > make.log
 
