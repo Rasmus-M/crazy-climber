f2dcca0a872d79f0d0616d28e7b222210f02f486
Rasmus Moustgaard <rasmus.moustgaard@gmail.com>
2025-09-13 11:39:17 +0200

    Moves graphics into ROM

--- make.bat
+++ make.bat
@@ -1,11 +1,12 @@
 xas99.py src/crazy-climber.a99 -i -q -R -L crazy-climber.lst -E symbols.txt -o bin/main
 xas99.py src/rom-bank-4.a99 -B -q -R -o bin/bank4.bin
+xas99.py src/rom-bank-5.a99 -B -q -R -o bin/bank5.bin
 
 java -jar tools/ea5tocart.jar bin\main "CRAZY CLIMBER"
 
 copy /b bin\main8.bin ^
     + bin\bank4.bin ^
-    + bin\empty.bin ^
+    + bin\bank5.bin ^
     + bin\empty.bin ^
     + bin\empty.bin ^
     crazy-climber8.bin
