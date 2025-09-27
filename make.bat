5eba5f8172aa5bb5bd88bb4fb3c8a3df3732d161
Rasmus Moustgaard <rasmus.moustgaard@gmail.com>
2025-09-27 18:55:29 +0200

    Added level 3 and 4 maps

--- make.bat
+++ make.bat
@@ -2,6 +2,7 @@ xas99.py src/crazy-climber.a99 -i -q -R -L crazy-climber.lst -E symbols.txt -o b
 xas99.py src/rom-bank-4.a99 -B -q -R -o bin/bank4.bin
 xas99.py src/rom-bank-5.a99 -B -q -R -o bin/bank5.bin
 xas99.py src/rom-bank-6.a99 -B -q -R -o bin/bank6.bin
+xas99.py src/rom-bank-7.a99 -B -q -R -o bin/bank7.bin
 
 java -jar tools/ea5tocart.jar bin\main "CRAZY CLIMBER"
 
@@ -9,7 +10,7 @@ copy /b bin\main8.bin ^
     + bin\bank4.bin ^
     + bin\bank5.bin ^
     + bin\bank6.bin ^
-    + bin\empty.bin ^
+    + bin\bank7.bin ^
     crazy-climber8.bin
 
 java -jar tools/CopyHeader.jar crazy-climber8.bin 60
