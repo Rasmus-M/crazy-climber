501c85020f87259e22da5f197d8590ec036838a3
Rasmus Moustgaard <rasmus.moustgaard@gmail.com>
2025-10-04 18:50:45 +0200

    All door signs

--- make.bat
+++ make.bat
@@ -3,6 +3,7 @@ xas99.py src/rom-bank-4.a99 -B -q -R -o bin/bank4.bin
 xas99.py src/rom-bank-5.a99 -B -q -R -o bin/bank5.bin
 xas99.py src/rom-bank-6.a99 -B -q -R -o bin/bank6.bin
 xas99.py src/rom-bank-7.a99 -B -q -R -o bin/bank7.bin
+xas99.py src/rom-bank-8.a99 -B -q -R -o bin/bank8.bin
 
 java -jar tools/ea5tocart.jar bin\main "CRAZY CLIMBER"
 
@@ -11,6 +12,14 @@ copy /b bin\main8.bin ^
     + bin\bank5.bin ^
     + bin\bank6.bin ^
     + bin\bank7.bin ^
+    + bin\bank8.bin ^
+    + bin\empty.bin ^
+    + bin\empty.bin ^
+    + bin\empty.bin ^
+    + bin\empty.bin ^
+    + bin\empty.bin ^
+    + bin\empty.bin ^
+    + bin\empty.bin ^
     crazy-climber8.bin
 
 java -jar tools/CopyHeader.jar crazy-climber8.bin 60
