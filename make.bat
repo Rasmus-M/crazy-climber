c8e720d73a893ad497b6eb602819d109ffc23beb
Rasmus Moustgaard <rasmus.moustgaard@gmail.com>
2025-10-24 19:30:23 +0200

    Entertainer music

--- make.bat
+++ make.bat
@@ -1,5 +1,5 @@
 xas99.py src/crazy-climber.a99 -i -q -R -L crazy-climber.lst -E symbols.txt -o bin/main
-xas99.py src/rom-banks/rom-bank-4.a99 -B -q -R -o bin/bank4.bin
+xas99.py src/rom-banks/rom-bank-4.a99 -B -q -R -o bin/bank4.bin -D SND_CHANNEL_1=">8396",SND_CHANNEL_2=">839c",SND_CHANNEL_3=">83a2",SND_CHANNEL_4=">83a8"
 xas99.py src/rom-banks/rom-bank-5.a99 -B -q -R -o bin/bank5.bin
 xas99.py src/rom-banks/rom-bank-6.a99 -B -q -R -o bin/bank6.bin
 xas99.py src/rom-banks/rom-bank-7.a99 -B -q -R -o bin/bank7.bin
