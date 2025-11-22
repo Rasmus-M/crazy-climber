e73ef9cc6a8bfe7a631c16b595295457c70b0c2a
Rasmus Moustgaard <rasmus.moustgaard@gmail.com>
2025-11-22 11:06:29 +0100

    Speech

---
+++ audio/speech/convert.bat
@@ -0,0 +1,13 @@
+set CLASSPATH=tools/out/videotools.jar
+
+java ConvertWavToLpc -maxfrequency 510 -preemphasis 0.82 -trimsilenceframes -addstopframe go-for-it.wav go-for-it.lpc
+java ConvertWavToLpc -trimsilenceframes -addstopframe ouch.wav ouch.lpc
+java ConvertWavToLpc -trimsilenceframes -addstopframe oh-no.wav oh-no.lpc
+java ConvertWavToLpc -maxfrequency 400 -trimsilenceframes -addstopframe extra-life.wav extra-life.lpc
+java ConvertWavToLpc -maxfrequency 500 -trimsilenceframes -addstopframe yuck.wav yuck.lpc
+
+java ConvertLpcToWav go-for-it.lpc test-go-for-it.wav
+java ConvertLpcToWav ouch.lpc test-ouch.wav
+java ConvertLpcToWav oh-no.lpc test-oh-no.wav
+java ConvertLpcToWav extra-life.lpc test-extra-life.wav
+java ConvertLpcToWav yuck.lpc test-yuck.wav
